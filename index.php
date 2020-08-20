5bb086e608ab1ac392b7a0cb668e6f451a8b749f
Rishabh Verma <rvstowrn@gmail.com>
2020-08-20 12:20:31 +0530

    structuring project

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Login Page</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://fonts.googleapis.com/css?family=Roboto+Mono" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="./styles/index-style.css" />
+</head>
+<body>
+  <main>
+    <ul>
+      <li style="font-size:29px;">login</li>
+      <form>
+        <li><b>username</b> <input type="text" placeholder="Enter Username" name="uname" required></li>
+        <li><b>password</b> <input type="password" placeholder="Enter Password" name="psw" required></li>
+        <li><button type="submit">let me in</button></li>
+      </form>
+    </ul>
+  </main>
+  <img src="./assets/blog.jpg">
+</body>
+</html>
